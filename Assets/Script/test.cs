3eb81e13dc369f20210ff609ccfd38986af1be23
QuyenChiLong-00 <tuanfaker00@gmail.com>
2025-03-15 15:57:53 +0700

    Update test.cs

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -5,7 +5,7 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("testcc");
     }
 
     // Update is called once per frame
