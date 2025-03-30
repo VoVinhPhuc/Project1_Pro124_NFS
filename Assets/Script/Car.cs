15bf3f86404a580ed88c79424fdd9e94807f318e
VoVinhPhuc <vovinhphuc2001@gmail.com>
2025-03-30 13:11:38 +0700

    Update_game300325

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    public float acceleration = 10f;
-    public float maxSpeed = 15f;
-    public float turnSpeed = 200f;
-    public float driftFactor = 0.95f;
-    public float nitroBoost = 1.5f;
-    public float brakeForce = 0.5f;
-    public float skillCooldown = 3f;
+    public float acceleration = 5f; // 5
+    public float maxSpeed = 7f; // 7 
+    public float turnSpeed = 120f; // 120 
+    public float driftFactor = 120f; // 120 
+    public float nitroBoost = 10f; // 10 
+    public float brakeForce = 1f; // 1 
+    public float skillCooldown = 3f; // 3
+    public float stopThreshold = 10f; // 10 
 
     private Rigidbody2D rb;
     private float moveInput;
@@ -30,18 +31,24 @@ public class Car : MonoBehaviour
         isNitro = Input.GetKey(KeyCode.LeftShift);
 
         if (Input.GetKey(KeyCode.Space))
+        {
             rb.linearVelocity *= brakeForce;
+        }
 
         HandleDrift();
 
         if (Input.GetKeyDown(KeyCode.F) && canUseSkill)
+        {
             StartCoroutine(UseSkill());
+        }
+            
     }
 
     void FixedUpdate()
     {
         ApplyEngineForce();
         ApplySteering();
+        ApplyStop();
     }
 
     void ApplyEngineForce()
@@ -73,6 +80,14 @@ public class Car : MonoBehaviour
 
         if (isDrifting)
             rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, transform.up * rb.linearVelocity.magnitude, driftFactor * Time.deltaTime);
+    }
+    void ApplyStop()
+    {
+        if (moveInput == 0 && rb.linearVelocity.magnitude < stopThreshold)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
     }
 
     IEnumerator UseSkill()
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private float rotateOffset = 180f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void RotateGun()
+    {
+        // Vector 3 displacement = transform.posision - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // float angle = Mathf 
+    }
+}
